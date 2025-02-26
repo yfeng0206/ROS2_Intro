9ab9613432788c444ed7461911576ac5f8bb98ac
Gary Feng <garyfeng000@gmail.com>
2025-02-26 07:00:16 +0000

    Added yaml for control

--- install/bumperbot_description/share/bumperbot_description/launch/gazebo.launch.py
+++ install/bumperbot_description/share/bumperbot_description/launch/gazebo.launch.py
@@ -10,13 +10,19 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
     bumperbot_des_description= get_package_share_directory("bumperbot_description")
+    ros_distro = os.environ["ROS_DISTRO"]
+    is_ignition = "True" if ros_distro =="humble" else "False"
 
     model_arg = DeclareLaunchArgument(name="model", default_value=os.path.join(
         bumperbot_des_description, "urdf", "bumperbot.urdf.xacro"),
         description="Absolute path to robot URDF file"
     )
 
-    robot_description = ParameterValue(Command(["xacro ", LaunchConfiguration("model")]), value_type=str)
+    robot_description = ParameterValue(Command(["xacro ", 
+                                                LaunchConfiguration("model"),
+                                                " is_ignition:=",
+                                                is_ignition
+                                                ]), value_type=str)
 
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
