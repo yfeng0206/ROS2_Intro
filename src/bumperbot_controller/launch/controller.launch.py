024e11a16cd600258b1e14b0294a6fbc24150bb5
Gary Feng <garyfeng000@gmail.com>
2025-03-25 05:59:48 +0000

    added launch files

--- src/bumperbot_controller/launch/controller.launch.py
+++ src/bumperbot_controller/launch/controller.launch.py
@@ -1,8 +1,29 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import UnlessCondition
 
 def generate_launch_description():
+
+    use_python_arg = DeclareLaunchArgument(
+        "use_python", 
+        default_value="False"
+    )
+
+    wheel_radius_arg = DeclareLaunchArgument(
+        "wheel_radius", 
+        default_value="0.033"
+    )
+
+    wheel_separation_arg = DeclareLaunchArgument(
+        "wheel_separation", 
+        default_value="0.17"
+    )
+
+    use_python = LaunchConfiguration("use_python")
+    wheel_radius = LaunchConfiguration("wheel_radius")
+    wheel_separation = LaunchConfiguration("wheel_separation")
 
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
@@ -23,8 +44,20 @@ def generate_launch_description():
         ]
     )
 
+    simple_controller_cpp = Node(
+        package="bumperbot_controller",
+        executable="simple_controller",
+        parameters=[{"wheel_radius": wheel_radius,
+                    "wheel_separation": wheel_separation}],
+        condition = UnlessCondition(use_python)
+    )
+
     return LaunchDescription(
         [
+            use_python_arg,
+            wheel_radius_arg,
+            wheel_separation_arg,
+            simple_controller_cpp,
             joint_state_broadcaster_spawner,
             simple_controller,
         ]
